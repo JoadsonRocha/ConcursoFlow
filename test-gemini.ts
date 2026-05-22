55c362829037afd32c016253aacaddbce7009a79
Joadson Rocha <joadsonrocharr@gmail.com>
2026-05-22 06:10:20 -0400

    docker

--- test-gemini.ts
+++
@@ -1,9 +0,0 @@
-import { generateMindMap } from './server/services/gemini.js';
-(async () => {
-  try {
-    const res = await generateMindMap("Matemática");
-    console.log(res);
-  } catch (err) {
-    console.error("DEBUG ERR:", err);
-  }
-})();
--- test-noauth.ts
+++
@@ -1,9 +0,0 @@
-import admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore';
-
-try {
-  const db = getFirestore();
-  console.log("Firestore initialized?");
-} catch (e) {
-  console.error("Firestore failed:", e);
-}
